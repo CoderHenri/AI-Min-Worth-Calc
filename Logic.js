628e9f76a7b6eab350b461ed2e9ce7f0764e13b0
Henri0812 <p.metzner@gmx.at>
2020-03-18 18:52:36 +0100

    ETH Input condition

--- Logic.js
+++ Logic.js
@@ -1,3 +1,13 @@
 function SelectAddress() {
-
+    var txt;
+    var PopUp = prompt("Please enter your ETH Address with your Axies:", "0x...");
+    if (PopUp == null || PopUp == "") 
+    {
+        txt = "User cancelled the prompt.";
+    } else if (PopUp.startsWith("0x") && PopUp.length == 42) {
+        txt = PopUp;
+    } else {
+        txt = "Please enter a real ETH Address (starts with 0x... and has 42 Characters)";
+    }
+    document.getElementById("ETHAddress").innerHTML = txt;
 }
