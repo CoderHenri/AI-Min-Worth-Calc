5f55b86759ac5ff5b43795d2e08e62c01f016594
Henri0812 <p.metzner@gmx.at>
2020-03-18 19:03:15 +0100

    second button dis-/enable

--- Logic.js
+++ Logic.js
@@ -4,10 +4,19 @@ function SelectAddress() {
     if (PopUp == null || PopUp == "") 
     {
         txt = "User cancelled the prompt.";
+        document.getElementById("WorthCalculator").disabled = true;
     } else if (PopUp.startsWith("0x") && PopUp.length == 42) {
         txt = PopUp;
+        document.getElementById("WorthCalculator").disabled = false; 
     } else {
         txt = "Please enter a real ETH Address (starts with 0x... and has 42 Characters)";
+        document.getElementById("WorthCalculator").disabled = true;
     }
     document.getElementById("ETHAddress").innerHTML = txt;
+}
+
+function CalculateWorth() {
+    var Address;
+    Address = document.getElementById("ETHAddress").innerHTML;
+    alert(Address);
 }
