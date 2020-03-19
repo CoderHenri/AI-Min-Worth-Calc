33bfa6e554a4ae2fbe61b68c129a34eab8dddd56
Henri0812 <p.metzner@gmx.at>
2020-03-19 18:54:32 +0100

    got it working as it should

--- Logic.js
+++ Logic.js
@@ -1,4 +1,11 @@
+var FirstOnclickCheck = 0; //stops the 'Start Calculation button from always substracting Axies from the Amount if the button is clicked more than once
+
 function SelectAddress() {
+  FirstOnclickCheck = 0; 
+  
+  document.getElementById("AccountCalculator").disabled = true;
+  document.getElementById("AccountCalculator").style.cursor = "not-allowed";
+
     var txt;
     var PopUp = prompt("Please enter your ETH Address with your Axies:", "0x...");
     if (PopUp == null || PopUp == "") 
@@ -16,37 +23,157 @@ function SelectAddress() {
         document.getElementById("AxieFloorButton").style.cursor = "not-allowed";
     }
     document.getElementById("ETHAddress").innerHTML = txt;
+
+    document.getElementById("PlayerOwned").innerHTML = 'Please Click the "Get Prices & Axies" Button';
+    document.getElementById("AccountWorthField").innerHTML = 'Please Click the "Get Prices & Axies" Button';
+    
 }
 
 function GetAxieFloor() {
+  FirstOnclickCheck = 0;
+
+  document.getElementById("AccountCalculator").disabled = true;
+  document.getElementById("AccountCalculator").style.cursor = "not-allowed";
+
     var Address;
     Address = document.getElementById("ETHAddress").innerHTML;
     alert(Address);
 
     
+    var PriceNormalURL = "https://axieinfinity.com/api/v2/axies?sale=1&sorting=lowest_price";
+    getAxiePrice(PriceNormalURL, "NormalAxiePrice");
+    var PriceOriginURL = "https://axieinfinity.com/api/v2/axies?title=Origin&sale=1&sorting=lowest_price";
+    getAxiePrice(PriceOriginURL, "OriginAxiePrice");
+    var PriceMystic1URL = "https://axieinfinity.com/api/v2/axies?mystic=true&num_mystic=1&offset=0&sale=1&sorting=lowest_price";
+    getAxiePrice(PriceMystic1URL, "Mystic1AxiePrice");
+    var PriceMystic2URL = "https://axieinfinity.com/api/v2/axies?mystic=true&num_mystic=2&offset=0&sale=1&sorting=lowest_price";
+    getAxiePrice(PriceMystic2URL, "Mystic2AxiePrice");
+    var PriceMystic3URL = "https://axieinfinity.com/api/v2/axies?mystic=true&num_mystic=3&offset=0&sale=1&sorting=lowest_price";
+    getAxiePrice(PriceMystic3URL, "Mystic3AxiePrice");
+    var PriceMystic4URL = "https://axieinfinity.com/api/v2/axies?mystic=true&num_mystic=4&offset=0&sale=1&sorting=lowest_price";
+    getAxiePrice(PriceMystic4URL, "Mystic4AxiePrice");
+    var PriceMEOURL = "https://axieinfinity.com/api/v2/axies?title=meo%20corp&sale=1&sorting=lowest_price";
+    getAxiePrice(PriceMEOURL, "MEOAxiePrice");
 
-    var PriceNormalVirinURL;
-
-    var PriceNormalNonVirginURL;
-    var PriceOriginURL;
-    var PriceMystic1URL = "https://axieinfinity.com/api/v2/axies?mystic=true&offset=1&sale=1&sorting=lowest_price";
-    getAxiePrice(PriceMystic1URL);
-    var PriceMystic2URL;
-    var PriceMystic3URL;
-    var PriceMystic4URL;
-    var PriceMEOURL;
-
-  async function getAxiePrice(url) {
+  async function getAxiePrice(url, id) {
     const resp = await fetch(url);
     const data = await resp.json()
     var Price = data.axies[0].auction.buyNowPrice;
     Price = Price / Math.pow(10, 18);
-    console.log(Price);
-    document.getElementById("Mystic1AxiePrice").innerHTML = Price + " ETH";
+    Price = Math.round((Price + 0.0000001) * 10000) / 10000
+    document.getElementById(id).innerHTML = Price + " ETH";
   }
+
+    var AmountNormalURL = "https://axieinfinity.com/api/v2/addresses/" + Address + "/axies?";
+    getAxieAmount(AmountNormalURL, "NormalAxieAmount");
+    var AmountOriginURL = "https://axieinfinity.com/api/v2/addresses/" + Address + "/axies?title=Origin";
+    getAxieAmount(AmountOriginURL, "OriginAxieAmount");
+    var AmountMystic1URL = "https://axieinfinity.com/api/v2/addresses/" + Address + "/axies?mystic=true&num_mystic=1";
+    getAxieAmount(AmountMystic1URL, "Mystic1AxieAmount");
+    var AmountMystic2URL = "https://axieinfinity.com/api/v2/addresses/" + Address + "/axies?mystic=true&num_mystic=2";
+    getAxieAmount(AmountMystic2URL, "Mystic2AxieAmount");
+    var AmountMystic3URL = "https://axieinfinity.com/api/v2/addresses/" + Address + "/axies?mystic=true&num_mystic=3";
+    getAxieAmount(AmountMystic3URL, "Mystic3AxieAmount");
+    var AmountMystic4URL = "https://axieinfinity.com/api/v2/addresses/" + Address + "/axies?mystic=true&num_mystic=4";
+    getAxieAmount(AmountMystic4URL, "Mystic4AxieAmount");
+    var AmountMEOURL = "https://axieinfinity.com/api/v2/addresses/" + Address + "/axies?title=meo%20corp";
+    getAxieAmount(AmountMEOURL, "MEOAxieAmount");
+
+  async function getAxieAmount(url, id) {
+    const resp = await fetch(url);
+    const data = await resp.json()
+    var Amount = data.totalAxies;
+    document.getElementById(id).innerHTML = Amount + " Axies";
+  }
+
+  document.getElementById("PlayerOwned").innerHTML = 'Please Click the "Start Calculation" Button to correct the Amount of Total to Normal Axies and start the Calculation';
+  document.getElementById("AccountWorthField").innerHTML = 'Please Click the "Start Calculation" Button to correct the Amount of Total to Normal Axies and start the Calculation';
+
+
+  setTimeout(function(){
+    document.getElementById("AccountCalculator").disabled = false;
+    document.getElementById("AccountCalculator").style.cursor = "default"; 
+  }, 3000);
 }
 
+function CalcAccWorth() {
 
+  if(FirstOnclickCheck == 0) {
+    AxieCountCorrector();
+    FirstOnclickCheck = 1;
+  }
+
+  var NormAxieWorth;
+  NormAxieWorth = Number(document.getElementById("NormalAxiePrice").innerHTML.replace(/[^0-9\.]+/g,"")) * 
+  parseFloat(document.getElementById("NormalAxieAmount").innerHTML.replace(/[^0-9]/g,''));
+  console.log(parseFloat(document.getElementById("NormalAxiePrice").innerHTML.replace(/[^0-9]/g,'')));
+  NormAxieWorth = Math.round((NormAxieWorth + 0.0000001) * 10000) / 10000;
+  document.getElementById("NormalAxieWorth").innerHTML = NormAxieWorth + " ETH";
+
+  var OriginAxieWorth;
+  OriginAxieWorth = Number(document.getElementById("OriginAxiePrice").innerHTML.replace(/[^0-9\.]+/g,"")) * 
+  parseFloat(document.getElementById("OriginAxieAmount").innerHTML.replace(/[^0-9]/g,''));
+  console.log(parseFloat(document.getElementById("OriginAxiePrice").innerHTML.replace(/[^0-9]/g,'')));
+  OriginAxieWorth = Math.round((OriginAxieWorth + 0.0000001) * 10000) / 10000;
+  document.getElementById("OriginAxieWorth").innerHTML = OriginAxieWorth + " ETH";
+
+  var MEOAxieWorth;
+  MEOAxieWorth = Number(document.getElementById("MEOAxiePrice").innerHTML.replace(/[^0-9\.]+/g,"")) * 
+  parseFloat(document.getElementById("MEOAxieAmount").innerHTML.replace(/[^0-9]/g,''));
+  console.log(parseFloat(document.getElementById("MEOAxiePrice").innerHTML.replace(/[^0-9]/g,'')));
+  MEOAxieWorth = Math.round((MEOAxieWorth + 0.0000001) * 10000) / 10000;
+  document.getElementById("MEOAxieWorth").innerHTML = MEOAxieWorth + " ETH";
+
+  var Mystic1AxieWorth;
+  Mystic1AxieWorth = Number(document.getElementById("Mystic1AxiePrice").innerHTML.replace(/[^0-9\.]+/g,"")) * 
+  parseFloat(document.getElementById("Mystic1AxieAmount").innerHTML.replace(/[^0-9]/g,''));
+  console.log(parseFloat(document.getElementById("Mystic1AxiePrice").innerHTML.replace(/[^0-9]/g,'')));
+  Mystic1AxieWorth = Math.round((Mystic1AxieWorth + 0.0000001) * 10000) / 10000;
+  document.getElementById("Mystic1AxieWorth").innerHTML = Mystic1AxieWorth + " ETH";
+
+  var Mystic2AxieWorth;
+  Mystic2AxieWorth = Number(document.getElementById("Mystic2AxiePrice").innerHTML.replace(/[^0-9\.]+/g,"")) * 
+  parseFloat(document.getElementById("Mystic2AxieAmount").innerHTML.replace(/[^0-9]/g,''));
+  console.log(parseFloat(document.getElementById("Mystic2AxiePrice").innerHTML.replace(/[^0-9]/g,'')));
+  Mystic2AxieWorth = Math.round((Mystic2AxieWorth + 0.0000001) * 10000) / 10000;
+  document.getElementById("Mystic2AxieWorth").innerHTML = Mystic2AxieWorth + " ETH";
+
+  var Mystic3AxieWorth;
+  Mystic3AxieWorth = Number(document.getElementById("Mystic3AxiePrice").innerHTML.replace(/[^0-9\.]+/g,"")) * 
+  parseFloat(document.getElementById("Mystic3AxieAmount").innerHTML.replace(/[^0-9]/g,''));
+  console.log(parseFloat(document.getElementById("Mystic3AxiePrice").innerHTML.replace(/[^0-9]/g,'')));
+  Mystic3AxieWorth = Math.round((Mystic3AxieWorth + 0.0000001) * 10000) / 10000;
+  document.getElementById("Mystic3AxieWorth").innerHTML = Mystic3AxieWorth + " ETH";
+
+  var Mystic4AxieWorth;
+  Mystic4AxieWorth = Number(document.getElementById("Mystic4AxiePrice").innerHTML.replace(/[^0-9\.]+/g,"")) * 
+  parseFloat(document.getElementById("Mystic4AxieAmount").innerHTML.replace(/[^0-9]/g,''));
+  console.log(parseFloat(document.getElementById("Mystic4AxiePrice").innerHTML.replace(/[^0-9]/g,'')));
+  Mystic4AxieWorth = Math.round((Mystic4AxieWorth + 0.0000001) * 10000) / 10000;
+  document.getElementById("Mystic4AxieWorth").innerHTML = Mystic4AxieWorth + " ETH";
+}
+
+function AxieCountCorrector() {
+  var TotalAxies = parseFloat(document.getElementById("NormalAxieAmount").innerHTML.replace(/[^0-9]/g,''));
+  var NonNormalAxies = parseFloat(document.getElementById("OriginAxieAmount").innerHTML.replace(/[^0-9]/g,'')) + parseFloat(document.getElementById("MEOAxieAmount").innerHTML.replace(/[^0-9]/g,'')) +
+  parseFloat(document.getElementById("Mystic1AxieAmount").innerHTML.replace(/[^0-9]/g,'')) + parseFloat(document.getElementById("Mystic2AxieAmount").innerHTML.replace(/[^0-9]/g,'')) + 
+  parseFloat(document.getElementById("Mystic3AxieAmount").innerHTML.replace(/[^0-9]/g,'')) + parseFloat(document.getElementById("Mystic4AxieAmount").innerHTML.replace(/[^0-9]/g,''));
+  var NormalAxies = TotalAxies - NonNormalAxies;
+  document.getElementById("NormalAxieAmount").innerHTML = NormalAxies + " Axies";
+
+  var TotalOriginAxies = parseFloat(document.getElementById("OriginAxieAmount").innerHTML.replace(/[^0-9]/g,''));
+  var MysticAxies = parseFloat(document.getElementById("Mystic1AxieAmount").innerHTML.replace(/[^0-9]/g,'')) + parseFloat(document.getElementById("Mystic2AxieAmount").innerHTML.replace(/[^0-9]/g,'')) + 
+  parseFloat(document.getElementById("Mystic3AxieAmount").innerHTML.replace(/[^0-9]/g,'')) + parseFloat(document.getElementById("Mystic4AxieAmount").innerHTML.replace(/[^0-9]/g,''));
+  var OriginAxies = TotalOriginAxies - MysticAxies;
+  document.getElementById("OriginAxieAmount").innerHTML = OriginAxies + " Axies";
+
+}
+
+//Amount of the Axies owned by the Player
+
+//Please Click the "Start Calculation" Button
+
+//Minimum Worth of the Account according to price floor of the Axie Marketplace
 
 
 
